39f9b527d8ce78bddbbae3159c4bbb67b1bb4567
faize3 <faizee.shk@gmail.com>
2019-01-07 21:38:43 +0530

    docs

---
+++ docs/precache-manifest.3956ffb45a0bf11c3e8e45d48d8efbbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "96ddc148c0a83900768d",
+    "url": "/static/js/main.96ddc148.chunk.js"
+  },
+  {
+    "revision": "fb742f118a5eed1ad517",
+    "url": "/static/js/1.fb742f11.chunk.js"
+  },
+  {
+    "revision": "96ddc148c0a83900768d",
+    "url": "/static/css/main.1768234e.chunk.css"
+  },
+  {
+    "revision": "ec1a7fcae6cd356d6b082198a63c43bd",
+    "url": "/index.html"
+  }
+];
